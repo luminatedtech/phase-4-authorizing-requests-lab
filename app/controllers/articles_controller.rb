8b7100dc398889d8b7c7e32c345014a43a58eae6
Allen Kim <avalmercafe02@gmail.com>
2023-05-03 15:20:54 -0400

    commit tree 2772d24328394c1e6ac15bf93f4b7484fcacd739

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
+  before_action :authorize 
   def index
     articles = Article.where(is_member_only: false).includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
@@ -15,6 +15,9 @@ class ArticlesController < ApplicationController
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
+  end
+  def authorize 
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
   end
 
 end
